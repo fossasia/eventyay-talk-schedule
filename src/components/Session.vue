680759b478b9b663a6f3bf0e49954935012f3c9c
lcduong <lcduong@tma.com.vn>
2024-07-31 18:01:46 +0700

    re-enable favs count in session list view

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -26,7 +26,7 @@ a.c-linear-schedule-session(:class="{faved}", :style="style", :href="link", @cli
 		.bottom-info
 			.track(v-if="session.track") {{ getLocalizedString(session.track.name) }}
 			.room(v-if="showRoom && session.room") {{ getLocalizedString(session.room.name) }}
-
+		.fav-count(v-if="session.fav_count > 0 && isLinearSchedule") {{ session.fav_count > 99 ? "99+" : session.fav_count  }}
 	bunt-icon-button.btn-fav-container(@click.prevent.stop="faved ? $emit('unfav', session.id) : $emit('fav', session.id)")
 		svg.star(viewBox="0 0 24 24")
 			path(d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z")
@@ -60,7 +60,8 @@ export default {
 		hasAmPm: {
 			type: Boolean,
 			default: false
-		}
+		},
+		isLinearSchedule: Boolean
 	},
 	inject: {
 		eventUrl: { default: null },
