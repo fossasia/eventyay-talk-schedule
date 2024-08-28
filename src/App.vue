a3774d894903afbbd815342f1f7713eda26cb1c3
Tobias Kunze <r@rixx.de>
2024-08-28 13:37:14 +0200

    Fix timezone indicator some more
    sigh @ magical calc removal in the vue2 build, should upgrade asap

--- src/App.vue
+++ src/App.vue
@@ -556,7 +556,7 @@ export default {
 		align-items: center
 		z-index: 100
 		left: 18px
-		width: min(calc(100% - 36px), var(--schedule-max-width))
+		width: calc(100% - 36px)
 		.fav-toggle
 			display: flex
 			margin-right: 5px
@@ -589,7 +589,6 @@ export default {
 			color: $clr-secondary-text-light
 		.timezone-item
 			margin-left: auto
-			padding-right: 28px
 	.days
 		background-color: $clr-white
 		tabs-style(active-color: var(--pretalx-clr-primary), indicator-color: var(--pretalx-clr-primary), background-color: transparent)
